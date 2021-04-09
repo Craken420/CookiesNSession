84aa8980f6f4866450e842ab725bf5bd5917ea2e
Luis Angel Peña <lapena@mavi.mx>
2021-04-09 18:59:26 -0500

    feat:: Add the principal routing path with the session form

--- src/app/routes/session-routes.js
+++ src/app/routes/session-routes.js
@@ -30,4 +30,38 @@ if (router.get('env') === 'production') {
 
 router.use( session(sessOpc) );
 
+router.get('/', (req, res) => {
+    // Parse the cookies on the request
+    const cookies = cookie.parse(req.headers.cookie || '');
+ 
+    // Get the visitor name set in the cookie
+    const sessionid = cookies['session-id'];
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write(`<h1>Welcome</h1>
+        <p>Cookies: ${JSON.stringify(cookies)}</p>
+        <p>Session: ${(req.session.user||'visitor')}</p>
+        <p>Cookies.session-id:  ${(sessionid || 'undefined') }</p>`);
+
+    if (req.session.views) {
+        req.session.views++;
+        res.write(`<p>Session Views: ${req.session.views}</p>`);
+    } else
+        req.session.views = 1;
+    
+    if (req.session.user)
+        res.write(`<a href="/session/Content">Content</a><br/>
+                   <a href="/session/logout">Logout</a>`);
+    else {
+        req.session.views = 1;
+        res.write(`
+            <form method="GET" action="/session/login">
+                <input placeholder="enter your username" name="username">
+                <input placeholder="enter your password" name="password">
+                <input type="submit" value="Set session">
+            </form>`);
+    }
+    res.end();
+});
+
 module.exports = router;
