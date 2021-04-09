6a079af03e81afb15def8aa978617c5ae89fb39a
Luis Angel Peña <lapena@mavi.mx>
2021-04-09 11:11:23 -0500

    Get and set the cookies using the module cookie

--- src/app/app.js
+++ src/app/app.js
@@ -2,10 +2,14 @@ const morgan = require('morgan'),
       express = require('express'),
       app = express();
 
+const middleware = require('./middlewares/middlewares');
+
 app.use( express.urlencoded({extended: true}) );
 app.use( express.json() );
 app.use( morgan('dev') );
 
 app.set('PORT', 3003);
+
+app.use(middleware.onRequest);
 
 module.exports = app;
